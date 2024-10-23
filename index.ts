618708d92b5aa3c74a64a2a807753331c0b3412a
Andrii Konovalchuk <andrii.konovalchuk.work@gmail.com>
2024-10-23 15:43:00 +0300

    fixed conflict in module2

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log('Hello NodeJS!');
+// const fs = require("fs");
+// const fsPromises = require("fs").promises;
+// import User from "/user.";
+import { writeFile } from "fs/promises";
+import { User } from "./src/types/user.intrface";
+import { createUserCard } from "./src/static/createUserCard";
+
+const usersAPIUrl = "https://jsonplaceholder.typicode.com/users";
+(async () => {
+  const userData: User[] = await fetch(usersAPIUrl).then((res) => res.json());
+  for (let user of userData) {
+    const userObject = user as User;
+    await writeFile(
+      `./files/users/user-${userObject.id}.html`,
+      createUserCard(userObject)
+    );
+  }
+})();
