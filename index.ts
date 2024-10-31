4d128c07aab2f14f883b2151c88e14051229b73c
Kala4 <pershkovladyslav@gmail.com>
2024-10-31 12:23:04 +0200

    feat: accomplished module 6

--- index.ts
+++ index.ts
@@ -1,1 +1,23 @@
-console.log('Hello NodeJS!');
+import fs from 'fs';
+
+const writeStream = fs.createWriteStream('./files/output.txt')
+
+const readFirstInput = fs.createReadStream('./files/input-1.txt');
+
+readFirstInput.on('data', (chunk) => {
+    writeStream.write(chunk)
+});
+
+readFirstInput.on('end', () => {
+    console.log('Finished reading');
+});
+
+const readSecondInput = fs.createReadStream('./files/input-2.txt');
+
+readSecondInput.on('data', (chunk) => {
+    writeStream.write(chunk)
+});
+
+readSecondInput.on('end', () => {
+    console.log('Finished reading');
+});
